c0afebc11ca16cf5f6c6da4ac4c453f7551564b0
tgoky <tgokyicloud6@gmail.com>
2023-08-31 08:49:52 +0100

    chore:newWidth

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -43,28 +43,45 @@ const MemeGenerator = () => {
     if (selectedImage) {
       const canvas = createCanvas(selectedImage.width, selectedImage.height);
       const ctx = canvas.getContext('2d');
-  
+    
       const image = new Image();
       image.src = URL.createObjectURL(selectedImage);
-  
+    
       image.onload = () => {
-        canvas.width = image.width;
-        canvas.height = image.height;
+        // Define the maximum width and height for the resized image
+        const maxWidth = 600;
+        const maxHeight = 600;
+        
+        // Calculate the new dimensions while maintaining the aspect ratio
+        let newWidth = image.width;
+        let newHeight = image.height;
+        if (newWidth > maxWidth) {
+          newWidth = maxWidth;
+          newHeight = (image.height * maxWidth) / image.width;
+        }
+        if (newHeight > maxHeight) {
+          newHeight = maxHeight;
+          newWidth = (image.width * maxHeight) / image.height;
+        }
   
-        ctx.drawImage(image, 0, 0, image.width, image.height);
+        canvas.width = newWidth;
+        canvas.height = newHeight;
   
+        ctx.drawImage(image, 0, 0, newWidth, newHeight);
+    
         ctx.fillStyle = 'white';
         ctx.font = '30px Arial';
         ctx.textAlign = 'center';
-  
+    
         ctx.fillText(topText, canvas.width / 2, 40);
         ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
-  
+    
         const memeDataURL = canvas.toDataURL('image/png');
         document.getElementById('meme-preview').src = memeDataURL;
       };
     }
   };
+  
   
 
   return (
