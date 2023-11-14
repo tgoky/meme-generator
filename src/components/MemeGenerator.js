64dc7ee3854e3033fe6e4e7ec01b0f93dcd89f94
tgoky <tgokyicloud6@gmail.com>
2023-11-14 09:37:12 +0100

    chore:update

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,160 +1,86 @@
-
 import React, { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import { createCanvas } from 'canvas';
-import './Iris.css';
+import Accessories from './Accessory';
 
-import overlayImage1 from '../overlays/hotdog.png';
-import overlayImage2 from '../overlays/hotdog2.png';
-import overlayImage3 from '../overlays/hotdog3.png';
+const ProfilePictureEditor = () => {
+  const [image, setImage] = useState(null);
 
+  const onDrop = (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
 
-const ImageUpload = ({ onImageUpload }) => {
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: 'image/*',
-    onDrop: (acceptedFiles) => {
-      if (acceptedFiles.length > 0) {
-        onImageUpload(acceptedFiles[0]);
-      }
-    },
-  });
- 
+    reader.onload = () => {
+      setImage(reader.result);
+    };
 
-//elite
-//faith
-//caution
-//structure
-//structs
+    reader.readAsDataURL(file);
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className="image-upload-container" {...getRootProps()}>
-    <div className="image-upload">
-      <div className="image-upload-border">
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+      <div {...getRootProps()} style={dropzoneStyle}>
         <input {...getInputProps()} />
-        <p>Drag & drop an image here, or click to select one</p>
+        <p style={{color: 'white'}}>Drag 'n' drop your image here, or click to select a file</p>
       </div>
-    </div>
-  </div>
-  );
-};
 
-const TextInput = ({ label, value, onChange }) => {
-  return (
-    <div className="text-input">
-      <label>{label}</label>
-      <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />
-    </div>
-  );
-};
+      {image && (
+        <div style={imageContainerStyle}>
+          <img src={image} alt="Uploaded" style={imageStyle} />
+        </div>
+      )}
 
-const MemeGenerator = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const overlayImages = [overlayImage1, overlayImage2, overlayImage3];
+      {image && (
+        <div style={accessoryContainerStyle}>
 
-  const handleImageUpload = (image) => {
-    setSelectedImage(image);
-  };
+          <Accessories image={image} />
+          </div>
 
-  const handleOverlayImageDrop = (event, overlayIndex) => {
-    event.preventDefault();
-    // Handle the drop of overlay image at the specified index
-  };
-
-  
-  const handleGenerateMeme = () => {
-    if (selectedImage) {
-      const canvas = createCanvas(selectedImage.width, selectedImage.height);
-      const ctx = canvas.getContext('2d');
-    
-      const image = new Image();
-      image.src = URL.createObjectURL(selectedImage);
-    
-      image.onload = () => {
-        // Define the maximum width and height for the resized image
-        const maxWidth = 600;
-        const maxHeight = 600;
-        
-        // Calculate the new dimensions while maintaining the aspect ratio
-        let newWidth = image.width;
-        let newHeight = image.height;
-        if (newWidth > maxWidth) {
-          newWidth = maxWidth;
-          newHeight = (image.height * maxWidth) / image.width;
-        }
-        if (newHeight > maxHeight) {
-          newHeight = maxHeight;
-          newWidth = (image.width * maxHeight) / image.height;
-        }
-  
-        canvas.width = newWidth;
-        canvas.height = newHeight;
-  
-        ctx.drawImage(image, 0, 0, newWidth, newHeight);
-    
-        ctx.fillStyle = 'white';
-        ctx.font = '30px Arial';
-        ctx.textAlign = 'center';
-    
-        ctx.fillText(topText, canvas.width / 2, 40);
-        ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
-    
-        const memeDataURL = canvas.toDataURL('image/png');
-        document.getElementById('meme-preview').src = memeDataURL;
-      };
-    }
-  };
-
-  const handleGenerateMemeWithOverlay = () => {
-    if (selectedImage) {
-      const canvas = createCanvas(selectedImage.width, selectedImage.height);
-      const ctx = canvas.getContext('2d');
-
-      // Load uploaded base image
-      const baseImage = new Image();
-      baseImage.src = URL.createObjectURL(selectedImage);
-
-      baseImage.onload = () => {
-        // Apply overlay images on canvas
-        // Apply text and generate meme
-      };
-    }
-  };
-  
-  
-
-  return (
-    <div className="meme-generator">
-       <div className="overlay-images">
-        {overlayImages.map((overlayImage, index) => (
-          <img
-            key={index}
-            src={overlayImage}
-            width="80px"
-            height="80px"
-            alt={`Overlay ${index}`}
-            draggable="true"
-            onDragStart={(event) => event.dataTransfer.setData('text/plain', index)}
-          />
-        ))}
-      </div>
-      {/* ... Other components ... */}
-      <button onClick={handleGenerateMemeWithOverlay}>Generate Meme with Overlay</button>
-      <div className="meme-preview">
-        {/* Display combined meme image with overlay */}
-      </div>
-      <h1 style={{color: 'white'}}>GLIZZY OVERDRIVE</h1>
-      <ImageUpload onImageUpload={handleImageUpload} />
-      <TextInput label="Top Text" value={topText} onChange={setTopText} />
-      <TextInput label="Bottom Text" value={bottomText} onChange={setBottomText} />
-      <button onClick={handleGenerateMeme}>Generate Meme</button>
-      <div className="meme-preview">
-        <img id="meme-preview" alt="Meme Preview" style={{ width: 600, height: 480,}}/>
-      </div>
+      )}
     </div>
   );
 };
 
-export default MemeGenerator;
+const dropzoneStyle = {
+  width: '300px',
+  height: '200px', // Set a specific height for the dropzone
+  border: '2px dashed #cccccc',
+  borderRadius: '4px',
+  padding: '20px',
+  textAlign: 'center',
+  cursor: 'pointer',
+};
 
+const imageContainerStyle = {
+  marginTop: '20px', // Add space between dropzone and image
+  position: 'relative',
+};
 
+const imageStyle = {
+  width: '100%', // Ensure the image covers the entire container
+  height: '100%',
+  borderRadius: '4px',
+  border: '2px dashed #cccccc',
+  borderRadius: '4px',
+  padding: '20px',
+  textAlign: 'center',
+  cursor: 'pointer',
+};
+
+const accessoryContainerStyle = {
+  marginTop: '-990px',
+  marginLeft: '-1190px',
+  width: '200px',
+  height: '170px',
+  border: '2px dashed #cccccc',
+  borderRadius: '4px',
+  padding: '20px',
+  textAlign: 'center',
+  cursor: 'pointer',
+
+// Add space between image and accessory
+
+};
+
+export default ProfilePictureEditor;
